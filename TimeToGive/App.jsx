bd29c5a3c372fb1bbb1c4ea23c1ff112c2e6efba
paula <paula.pistrila@student.upt.ro>
2024-03-29 00:34:49 +0200

    Added screens folder, here you can find all 'screens' that you have in your app

--- TimeToGive/App.jsx
+++ TimeToGive/App.jsx
@@ -1,29 +1,11 @@
 import {Text, View, Image, TouchableOpacity, StyleSheet} from 'react-native'
-import { styles } from './styles/button';
-import { styles_page } from './styles/start_page';
-function App(){
-    return(
-    <View style={styles_page.container}>
-        <View>
-             <Text style={styles_page.welcomeText}>Welcome to our community!</Text>
-        </View>
-        <View >
-             <Image
-                    source={require('./photo/logo.jpg')}
-                    style={styles_page.logo}
-                  />
-        </View>
-        <View>
-             <TouchableOpacity style={styles.button}>
-                     <Text style={styles.buttonText}>LOGIN</Text>
-                   </TouchableOpacity>
-                   <TouchableOpacity style={styles.button}>
-                     <Text style={styles.buttonText}>REGISTER</Text>
-                   </TouchableOpacity>
-                   <Text style={styles.footerText}>Let's change this world together!</Text>
-        </View>
-    </View>
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomePage'
 
-    )
+function App(){
+    return <HomeScreen />
 }
 export default App;
+
+//mai jos incep deja sa fac partea de navigare printre ferestre
