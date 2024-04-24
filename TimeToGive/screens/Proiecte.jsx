a1e873ea2af8159a284e8064e86e502334e009cb
paula <paula.pistrila@student.upt.ro>
2024-04-24 20:33:13 +0300

    Vizionare proiecte

--- TimeToGive/screens/Proiecte.jsx
+++ TimeToGive/screens/Proiecte.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, View, Text, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, Image, TextInput, TouchableOpacity, ScrollView,FlatList } from 'react-native';
 import { iconStyles} from '../styles/icon';
 import { proiecteStyles} from '../styles/proiecte';
 import { styles } from '../styles/button';
@@ -8,118 +8,127 @@ import { separatorStyles} from '../styles/separator';
 import { containerStyles} from '../styles/container';
 import ImagePicker from 'react-native-image-crop-picker';
 import axios from 'axios';
+import moment from 'moment';
 
 function ProiecteScreen({ route, navigation }){
-  const userInfo = route.params?.userInfo;  
+   const userInfo = route.params?.userInfo;
+   const [projects, setProjects] = useState([]);
 
-  const [proiecte, setProiecte] = useState([]);
+const fetchProjects = async () => {
+    try {
+      const response = await fetch('http://10.0.2.2:5000/getProjects');
+      const json = await response.json();
+      if (response.ok) {
+        setProjects(json.data); // Asumăm că 'data' este array-ul cu proiecte
+      } else {
+        throw new Error('Failed to fetch projects');
+      }
+    } catch (error) {
+      console.error('Error fetching projects:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await axios.get('http://192.168.1.115:5000/getProjects');
-        setProiecte(response.data);
-      } catch (error) {
-        console.error('Error in taking over the projects from the database:', error);
-      }
-    };
-
     fetchProjects();
   }, []);
 
-  return (
-    <View style={containerStyles.container}>
-      <View style={footerStyles.header}>
-        <TouchableOpacity onPress={() => navigation.navigate("AdaugaProiect")}>
-               <Image
-                     source={require('../photo/add-button.png')}
-                     style={iconStyles.antetIcon}
-               />
-        </TouchableOpacity>
-        <Text style={footerStyles.headerTitle}>Proiecte</Text>
-        <TouchableOpacity >
-               <Image
-                     source={require('../photo/menu.png')}
-                     style={iconStyles.antetIcon}
-               />
-        </TouchableOpacity>
-      </View>
-      <TextInput style={footerStyles.searchBar} placeholder="Search" />
-
-     <ScrollView style={proiecteStyles.content}>
-       {Array.isArray(proiecte) && proiecte.map((proiect, index) => (
-         <View key={proiect._id} style={{ ...proiecteStyles.projectContainer }}>
-          {/*  <Image
-             source={proiect.image ? { uri: proiect.image } : require('../photo/no_photo.jpg')}
-             style={proiecteStyles.image}
-           /> */}
-           <Text style={proiecteStyles.eventTitle}>{proiect.projectName}</Text>
-
-           <View style={separatorStyles.separator} />
-
-           <View>
-             <Text style={proiecteStyles.text}>Description:</Text>
-             <Text style={proiecteStyles.descriptionText}>{proiect.description}</Text>
-           </View>
-
-           <View style={separatorStyles.separator} />
-
-           <View style={proiecteStyles.infoContainer}>
-             <Text style={proiecteStyles.infoText}>Event organizer: {proiect.organizer}</Text>
-             <Text style={proiecteStyles.infoText}>Email: {proiect.email}</Text>
-             <Text style={proiecteStyles.infoText}>Phone: {proiect.mobile}</Text>
-             <Text style={proiecteStyles.infoText}>Organization: {proiect.organization}</Text>
-             <Text style={proiecteStyles.infoText}>Country: {proiect.country}</Text>
-             <Text style={proiecteStyles.infoText}>Address: {proiect.address}</Text>
-             <Text style={proiecteStyles.infoText}>StartDate: {proiect.startDate}</Text>
-           </View>
-
-           <View style={separatorStyles.separator} />s
-
-           <TouchableOpacity style={styles.button}>
-             <Text style={proiecteStyles.applayText}>APPLY</Text>
-           </TouchableOpacity>
-
-           <View style={separatorStyles.separator} />
-         </View>
-       ))}
-     </ScrollView>
-
-
-      <View style={footerStyles.footer}>
-
-      <TouchableOpacity style={proiecteStyles.row} onPress={() => navigation.navigate("Profile", { userInfo: userInfo })}>
-
-                     <Image
-                           source={require('../photo/om_circle.png')}
-                           style={iconStyles.footerIcon}
-                     />
-                     <Text style={proiecteStyles.footerText}>Profile</Text>
-              </TouchableOpacity>
-
-
-
-        <TouchableOpacity style={proiecteStyles.row}  onPress={() => navigation.navigate("Organizations", { userInfo: userInfo })}>
-               <Image
-                     source={require('../photo/home_circle.png')}
-                     style={iconStyles.footerIcon}
-               />
-               <Text style={proiecteStyles.footerText}>Organizations</Text>
-        </TouchableOpacity>
-
-
-        <TouchableOpacity style={proiecteStyles.row} onPress={() => navigation.navigate("Proiecte", { userInfo: userInfo })}>
-               <Image
-                     source={require('../photo/leaf_circle.png')}
-                     style={iconStyles.footerIcon}
-               />
-               <Text style={proiecteStyles.footerText}>Projects</Text>
+   return (
+     <View style={containerStyles.container}>
+       <View style={footerStyles.header}>
+         <TouchableOpacity>
+                <Image
+                      source={require('../photo/add-button.png')}
+                      style={iconStyles.antetIcon}
+                />
          </TouchableOpacity>
-      </View>
+         <Text style={footerStyles.headerTitle}>Proiecte</Text>
+         <TouchableOpacity >
+                <Image
+                      source={require('../photo/menu.png')}
+                      style={iconStyles.antetIcon}
+                />
+         </TouchableOpacity>
+       </View>
+       <TextInput style={footerStyles.searchBar} placeholder="Search" />
 
-    </View>
-  );
-};
+
+          <FlatList
+              data={projects}
+              keyExtractor={(item) => item._id.toString()} // Presupunem că fiecare proiect are un _id unic
+              renderItem={({ item }) => (
+                <View>
+
+         <Image
+                        style={proiecteStyles.image}
+                  source={item.image === '' ? require('../photo/no_photo.jpg') : { uri: item.image }}
+                      />
+         <Text style={proiecteStyles.eventTitle}>{item.projectName}</Text>
+          <View style={separatorStyles.separator} />
+         <View >
+           <Text style={proiecteStyles.text}>Description:</Text>
+           <Text style={proiecteStyles.descriptionText}>{item.description}</Text>
+         </View>
+
+         <View style={separatorStyles.separator} />
+         <View style={proiecteStyles.AvailableContainer}>
+         <Text style={proiecteStyles.text}>Available spots: 3/27</Text>
+         <TouchableOpacity style={styles.button}>
+           <Text style={proiecteStyles.applayText}>APPLY</Text>
+         </TouchableOpacity>
+         </View>
+          <View style={separatorStyles.separator} />
+
+
+         <View style={proiecteStyles.infoContainer}>
+          <Text style={proiecteStyles.text}>Info:</Text>
+           <Text  style={proiecteStyles.infoText}>Event organizer: {item.organizer}</Text>
+           <Text  style={proiecteStyles.infoText}>Email: {item.email}</Text>
+           <Text  style={proiecteStyles.infoText}>Phone: {item.mobile}</Text>
+           <Text  style={proiecteStyles.infoText}>Organization: {item.organization}</Text>
+           <Text  style={proiecteStyles.infoText}>Country: {item.country}</Text>
+           <Text  style={proiecteStyles.infoText}>Address: {item.address}</Text>
+           <Text  style={proiecteStyles.infoText}>Start Date: {moment(item.startDate).format('MMMM Do YYYY')}</Text>
+           </View>
+ <View style={separatorStyles.separator} />
+         </View>
+   )}
+        style={proiecteStyles.content}
+      />
+
+       <View style={footerStyles.footer}>
+
+               <TouchableOpacity style={proiecteStyles.row} onPress={() => navigation.navigate("Profile", { userInfo: userInfo })} >
+                      <Image
+                            source={require('../photo/om_circle.png')}
+                            style={iconStyles.footerIcon}
+                      />
+                      <Text style={proiecteStyles.footerText}>Profile</Text>
+               </TouchableOpacity>
+
+
+
+         <TouchableOpacity style={proiecteStyles.row}  onPress={() => navigation.navigate("Organizations", { userInfo: userInfo })}>
+                <Image
+                      source={require('../photo/home_circle.png')}
+                      style={iconStyles.footerIcon}
+                />
+                <Text style={proiecteStyles.footerText}>Organizations</Text>
+         </TouchableOpacity>
+
+
+         <TouchableOpacity style={proiecteStyles.row} onPress={() => navigation.navigate("Proiecte", { userInfo: userInfo })}>
+                <Image
+                      source={require('../photo/leaf_circle.png')}
+                      style={iconStyles.footerIcon}
+                />
+                <Text style={proiecteStyles.footerText}>Projects</Text>
+          </TouchableOpacity>
+       </View>
+
+     </View>
+   );
+ };
+
 
 
 export default ProiecteScreen;
